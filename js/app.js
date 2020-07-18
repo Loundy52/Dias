d4cbc25a3d6c9a7b7914025788c5220cce97ff69
Barent Langwell <barent@Barents-MacBook-Pro.local>
2020-07-18 16:44:30 -0700

    [tictactoe] Add styles, js logic, html header

--- js/app.js
+++ js/app.js
@@ -6,9 +6,16 @@ console.log('Hello frontend');
 // }
 
 document.addEventListener('DOMContentLoaded', function() {
+    let moves = 0;
+    // if (moves % 2 === 0 && moves < 9) {  <- x/o logic
+        // square.textContent = 'o' 
+    // } else {
+        // square.textContent = 'x' 
+    // }
     leftTop = document.getElementById('left-top');
     leftTop.addEventListener('click', function(event) {
         leftTop.textContent = 'o' 
-        console.log('o')
+        console.log('o');
+        moves++;
     });
 })
