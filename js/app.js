20efcfda449b4aca235dbe4adf755f226591245e
Barent Langwell <barent@Barents-MacBook-Pro.local>
2020-07-18 15:24:09 -0700

    [tictactoe] Refactor html and css with grid model

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
 console.log('Hello frontend');
+
+// function moveO (element) {
+//     element.textContent = "o"
+//     console.log('o')
+// }
+
+document.addEventListener('DOMContentLoaded', function() {
+    leftTop = document.getElementById('left-top');
+    leftTop.addEventListener('click', function(event) {
+        leftTop.textContent = 'o' 
+        console.log('o')
+    });
+})
