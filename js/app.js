3a96d46867ea49414fe53e14c3d61aa545da341f
General Assembly <generalassembly@Davids-MacBook-Pro.local>
2020-07-19 11:41:13 -0700

    [major shift] Go from hidden move to appended move

--- js/app.js
+++ js/app.js
@@ -1,55 +1,64 @@
 document.querySelector("#leftT").addEventListener("click", onClick1);
-document.querySelector("#midT").addEventListener("click", onClick2);
-document.querySelector("#rightT").addEventListener("click", onClick3);
-document.querySelector("#leftM").addEventListener("click", onClick4);
-document.querySelector("#midM").addEventListener("click", onClick5);
-document.querySelector("#rightM").addEventListener("click", onClick6);
-document.querySelector("#leftB").addEventListener("click", onClick7);
-document.querySelector("#midB").addEventListener("click", onClick8);
-document.querySelector("#rightB").addEventListener("click", onClick9);
-
 
 function onClick1() {
-    if (X1.style.visiblity= "hidden")
-    document.querySelector("#X1").style.visibility = "visible";
+    var move1= document.querySelector("#leftT");
+    var xMove= document.createElement("h2");
+    xMove.textContent= "X";
+    move1.appendChild(xMove);
 }
+// document.querySelector("#midT").addEventListener("click", onClick2);
+// document.querySelector("#rightT").addEventListener("click", onClick3);
+// document.querySelector("#leftM").addEventListener("click", onClick4);
+// document.querySelector("#midM").addEventListener("click", onClick5);
+// document.querySelector("#rightM").addEventListener("click", onClick6);
+// document.querySelector("#leftB").addEventListener("click", onClick7);
+// document.querySelector("#midB").addEventListener("click", onClick8);
+// document.querySelector("#rightB").addEventListener("click", onClick9);
 
-function onClick2() {
-    if (X2.style.visiblity= "hidden")
-    document.querySelector("#X2").style.visibility = "visible";
-}
+// let oMove= document.createElement("h3");
+// oMove.textContent= "O";
 
-function onClick3() {
-    if (X3.style.visiblity= "hidden")
-    document.querySelector("#X3").style.visibility = "visible";
-}
+// function onClick1() {
+    //     if (X1.style.visiblity= "hidden")
+//     document.querySelector("#X1").style.visibility = "visible";
+// }
 
-function onClick4() {
-    if (X4.style.visiblity= "hidden")
-    document.querySelector("#X4").style.visibility = "visible";
-}
+// function onClick2() {
+//     if (X2.style.visiblity= "hidden")
+//     document.querySelector("#X2").style.visibility = "visible";
+// }
 
-function onClick5() {
-    if (X5.style.visiblity= "hidden")
-    document.querySelector("#X5").style.visibility = "visible";
-}
+// function onClick3() {
+//     if (X3.style.visiblity= "hidden")
+//     document.querySelector("#X3").style.visibility = "visible";
+// }
 
-function onClick6() {
-    if (X6.style.visiblity= "hidden")
-    document.querySelector("#X6").style.visibility = "visible";
-}
+// function onClick4() {
+//     if (X4.style.visiblity= "hidden")
+//     document.querySelector("#X4").style.visibility = "visible";
+// }
 
-function onClick7() {
-    if (X7.style.visiblity= "hidden")
-    document.querySelector("#X7").style.visibility = "visible";
-}
+// function onClick5() {
+//     if (X5.style.visiblity= "hidden")
+//     document.querySelector("#X5").style.visibility = "visible";
+// }
 
-function onClick8() {
-    if (X8.style.visiblity= "hidden")
-    document.querySelector("#X8").style.visibility = "visible";
-}
+// function onClick6() {
+//     if (X6.style.visiblity= "hidden")
+//     document.querySelector("#X6").style.visibility = "visible";
+// }
 
-function onClick9() {
-    if (X9.style.visiblity= "hidden")
-    document.querySelector("#X9").style.visibility = "visible";
-}
+// function onClick7() {
+//     if (X7.style.visiblity= "hidden")
+//     document.querySelector("#X7").style.visibility = "visible";
+// }
+
+// function onClick8() {
+//     if (X8.style.visiblity= "hidden")
+//     document.querySelector("#X8").style.visibility = "visible";
+// }
+
+// function onClick9() {
+//     if (X9.style.visiblity= "hidden")
+//     document.querySelector("#X9").style.visibility = "visible";
+// }
