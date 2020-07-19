d6afb86b88b3428b239982bc4f03c9a36e9e16af
Barent Langwell <barent@Barents-MacBook-Pro.local>
2020-07-18 21:58:45 -0700

    [tictactoe] Add logic to app.js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,19 @@
 // counter vars
 let moves = 1;
 let currentMove = '';
-
-
-// get all elems with square class and set up an event listener
+let oClickedSquareIds = [];
+let xClickedSquareIds = [];
 let gameMessage = document.querySelector('h2');
 let squareIdArray = document.querySelectorAll('div.square');
-squareIdArray.forEach(function(element) {
-    element.addEventListener('click', function() {
-        console.log(element);
-        element.classList.add('clicked');
-        element.textContent = whoseMoveIsIt(element);
+
+// main eventListener
+document.addEventListener('DOMContentLoaded', function() {
+    squareIdArray.forEach(function(element) {
+        element.addEventListener('click', function() {
+            console.log(element);
+            element.classList.add('clicked');
+            element.textContent = whoseMoveIsIt(element);
+        });
     });
 });
 
@@ -18,16 +21,27 @@ squareIdArray.forEach(function(element) {
 // DONE add class to marked cells to display separate players
 // DONE display message about which turn is being played
 // DONE fix fencepost error
+// DONE wrap line 10 event listener in DOMContentLoaded listener
 
-// TODO wrap line 10 event listener in DOMContentLoaded listener
+// TODO devise victory logic
+// TODO check for winner after each turn
 // TODO detect draw conditions (ties)
 // TODO detect winner with 3 in a row (determine set of winning combinations)
 // TODO linethrough winner w/ alert message / ask to play again
+// TODO catsgame alert conditions
 // TODO reset game-board
 
-function whoseMoveIsIt (element) {
+
+// RESET GAME LOGIC
+// squareIdArray.forEach(function (self) {
+//     self.textContent = ' ';
+//     return self;
+// });
+
+function whoseMoveIsIt(element) {
     // add 1 to move counter
     moves++;
+     // add element id to clickedSquares array
 
     // alternate between x's and o's
     if (moves % 2 === 0 && moves < 10) {  //  x/o logic
@@ -36,13 +50,18 @@ function whoseMoveIsIt (element) {
         element.textContent = 'o'; 
         currentMove = 'o';
         gameMessage.textContent = 'It\'s X\'s Turn';
+        oClickedSquareIds.push(element.id);
+        console.log(oClickedSquareIds);
+        
         
     } else {
 
         element.classList.add('playerX')
         element.textContent = 'x';
         currentMove = 'x';
-        gameMessage.textContent = 'It\'s O\'s Turn'
+        gameMessage.textContent = 'It\'s O\'s Turn';
+        xClickedSquareIds.push(element.id);
+        console.log(xClickedSquareIds);
     }
 
     // reset move counter and gameMessage after 9 iterations
@@ -53,13 +72,33 @@ function whoseMoveIsIt (element) {
         currentMove = 'o';
         moves = 1;
         gameMessage.textContent = 'Make a move!'; 
-    }
+        
+        // play again alert 
+
+    }  
 
     console.log(moves);
     console.log(currentMove);
     return element.textContent; // must return element.textContent for change to occur
     
 };
+
+// victory combo arrays
+
+// const leftVictory = clickedSquareIds.includes('left-top', 'left-middle', 'left-bottom');
+// clickedSquareIds.includes('center-top', 'center-middle', 'center-bottom');
+// clickedSquareIds.includes('right-top', 'right-middle', 'right-bottom');
+
+// if (leftVictory === true) {
+//     console.log('victory!!')
+// }
+
+// function determineWinner() {
+//     if (leftColumn.forEach() === 'o') {
+//         gameMessage.textContent = 'O Wins!';
+//         console.log('o wins!')
+//     }
+// };
 
 // document.addEventListener('DOMContentLoaded', function() {
 //     // if (moves % 2 === 0 && moves < 10) {  <- x/o logic
