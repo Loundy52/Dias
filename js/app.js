90d9462cf5ba985a070dd6f6719661c29c30745c
General Assembly <generalassembly@Davids-MacBook-Pro.local>
2020-07-19 16:24:30 -0700

    [move] Finally fix move

--- js/app.js
+++ js/app.js
@@ -4,18 +4,23 @@ function onClick1() {
     let checkO= document.querySelector("#O1");
     let move1= document.querySelector("#leftT");
     let xMove= document.createElement("h2");
-    xMove.setAttribute("id", "X1");
+        xMove.setAttribute("id", "X1");
     let xRemove= document.querySelector("#X1");
     let oMove= document.createElement("h3");
-    oMove.setAttribute("id", "O1");
-    if (checkX === null || checkO !== null) {
+        oMove.setAttribute("id", "O1");
+    let oRemove= document.querySelector("h3");
+    if (checkO !== null && checkX === null) {
+        move1.removeChild(oRemove);
         move1.appendChild(xMove);
         xMove.textContent= "X";
     } else if (checkX !== null && checkO === null) {
         move1.removeChild(xRemove);
         move1.appendChild(oMove);
         oMove.textContent= "O";
-    }   
+    } else if (checkX === null || checkO !== null) {
+        move1.appendChild(xMove);
+        oMove.textContent= "X";
+    }
 }
 
 // document.querySelector("#midT").addEventListener("click", onClick2);
@@ -27,48 +32,3 @@ function onClick1() {
 // document.querySelector("#midB").addEventListener("click", onClick8);
 // document.querySelector("#rightB").addEventListener("click", onClick9);
 
-
-// function onClick1() {
-    //     if (X1.style.visiblity= "hidden")
-//     document.querySelector("#X1").style.visibility = "visible";
-// }
-
-// function onClick2() {
-//     if (X2.style.visiblity= "hidden")
-//     document.querySelector("#X2").style.visibility = "visible";
-// }
-
-// function onClick3() {
-//     if (X3.style.visiblity= "hidden")
-//     document.querySelector("#X3").style.visibility = "visible";
-// }
-
-// function onClick4() {
-//     if (X4.style.visiblity= "hidden")
-//     document.querySelector("#X4").style.visibility = "visible";
-// }
-
-// function onClick5() {
-//     if (X5.style.visiblity= "hidden")
-//     document.querySelector("#X5").style.visibility = "visible";
-// }
-
-// function onClick6() {
-//     if (X6.style.visiblity= "hidden")
-//     document.querySelector("#X6").style.visibility = "visible";
-// }
-
-// function onClick7() {
-//     if (X7.style.visiblity= "hidden")
-//     document.querySelector("#X7").style.visibility = "visible";
-// }
-
-// function onClick8() {
-//     if (X8.style.visiblity= "hidden")
-//     document.querySelector("#X8").style.visibility = "visible";
-// }
-
-// function onClick9() {
-//     if (X9.style.visiblity= "hidden")
-//     document.querySelector("#X9").style.visibility = "visible";
-// }
