20fa5241f10d6475357d9acba8cee2697dbe050e
Elaine <elaine@Felipes-iMac.local>
2020-07-19 02:03:20 -0500

    [tictactoe] adds styling and html code

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
-console.log('Hello frontend');
+//Title construction
+//identifies title elements from html 
+const intro = document.querySelector("#intro")
+
+const second = document.querySelector("#second")
+
+const third = document.querySelector("#third")
+
+const final = document.querySelector("#final")
+//creates functions to add titles to elements
+function createIntro () {
+    intro.textContent = "Woah it's cold, almost like Winter's Coming.";
+};
+function createSecond () {
+    second.textContent = "What's that beyond the wall?";
+};
+function createThird () {
+    third.textContent = "Welp, time to call John Snow for an epic showdown.";
+};
+function createFourth () {
+    final.textContent = "Oh, actually eight seasons were really just building up to tic-tac-toe.";
+};
+//sets timing to begin title sequences
+function startTitle(create, time) {
+    setInterval(create, time);
+};
+
+startTitle(createIntro, 500);
+startTitle(createSecond, 900);
+startTitle(createThird, 1300);
+startTitle(createFourth, 1700);
+//document.addEventListener('DOMContentLoaded', startTitles);
