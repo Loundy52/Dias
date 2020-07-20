ef1ab19e690f2c421a2286d5ffd596d2a7a0beaf
Elaine <elaine@Felipes-iMac.local>
2020-07-20 11:02:16 -0500

    [codes] still some errors with play function

--- js/app.js
+++ js/app.js
@@ -29,4 +29,72 @@ startTitle(createIntro, 500);
 startTitle(createSecond, 900);
 startTitle(createThird, 1300);
 startTitle(createFourth, 1700);
-//document.addEventListener('DOMContentLoaded', startTitles);
+
+//Actual Assignment
+//create game play 
+//set event listeners for each square
+const winningCombos = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+
+
+const ex = 'js/JonSnow.png';
+const oh = 'js/NightWalker.png';
+let mark = 'js/JonSnow.png';
+let win;
+let gameboard;
+
+const grid = Array.from(document.querySelectorAll("img"));
+
+const messages = document.querySelector("h3");
+document.getElementById('mainElement').addEventListener('click', handleTurn);
+
+//determine if a win happened
+
+function score(){
+let player = null;
+winningCombos.forEach(function(combo, index){
+    if (gameboard[combo[0]] && gameboard[combo[0]] === gameboard[combo[1]] && gameboard[combo[0]] === gameboard[combo[2]]) winner = gameboard[combo[0]];
+});
+    return player ? player: gameboard.includes('') ? null : 'T';
+};
+
+//lot a turn's result
+
+function handleTurn() {
+    let idx = grid.findIndex(function(square) {
+        return square === event.target;
+    });
+    gameboard[idx] = mark;
+    mark = mark === ex ? oh : ex;
+    win = score();
+    play();
+};
+
+function start() {
+    gameboard = [
+    '', '', '',
+    '', '', '',
+    '', '', ''
+    ];
+    play();
+};
+
+function play() {
+    gameboard.forEach(function(mark, index) {
+    console.log(mark, index)
+    gameboard[index].textContent = mark; //some issue where it isn't marking into the grid array...
+    grid[index].src = mark;
+    });
+    messages.textContent = win === 'T' ? `Stalemate, wait for next season` : win ? `${win} wins the game!` : `It's ${mark}'s turn!`;
+    };
+start();
+console.log(grid);
+console.log(grid[2]);
