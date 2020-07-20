fad4e65d3a16492aef72881702fd35c4b76366f3
imjchiang <jchiang296@gmail.com>
2020-07-20 02:32:42 -0700

    [JS] event listeners, check wins, fix movement choice display

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var moveCount = 0;
-var boxes = [document.getElementById("1"), document.getElementById("2"), document.getElementById("3"), document.getElementById("4"), document.getElementById("5"), document.getElementById("6"), document.getElementById("7"), document.getElementById("8"), document.getElementById("9")];
-    
+const boxes = [document.getElementById("1"), document.getElementById("2"), document.getElementById("3"), document.getElementById("4"), document.getElementById("5"), document.getElementById("6"), document.getElementById("7"), document.getElementById("8"), document.getElementById("9")];
+
 function whoMove()
 {
     let message = document.querySelector("p");
@@ -34,7 +34,53 @@ function whatMove()
 
 function checkWins()
 {
-    
+    if (boxes[0].textContent !== "" && boxes[0].textContent === boxes[1].textContent && boxes[1].textContent === boxes[2].textContent)
+    {
+        console.log("Player " + boxes[0].textContent + " won!!!");
+    }
+    if (boxes[3].textContent !== "" && boxes[3].textContent === boxes[4].textContent && boxes[4].textContent === boxes[5].textContent)
+    {
+        console.log("Player " + boxes[3].textContent + " won!!!");
+    }
+    if (boxes[6].textContent !== "" && boxes[6].textContent === boxes[7].textContent && boxes[7].textContent === boxes[8].textContent)
+    {
+        console.log("Player " + boxes[6].textContent + " won!!!");
+    }
+
+    if (boxes[0].textContent !== "" && boxes[0].textContent === boxes[3].textContent && boxes[3].textContent === boxes[6].textContent)
+    {
+        console.log("Player " + boxes[0].textContent + " won!!!");
+    }
+    if (boxes[1].textContent !== "" && boxes[1].textContent === boxes[4].textContent && boxes[4].textContent === boxes[7].textContent)
+    {
+        console.log("Player " + boxes[1].textContent + " won!!!");
+    }
+    if (boxes[2].textContent !== "" && boxes[2].textContent === boxes[5].textContent && boxes[5].textContent === boxes[8].textContent)
+    {
+        console.log("Player " + boxes[2].textContent + " won!!!");
+    }
+
+    if (boxes[0].textContent !== "" && boxes[0].textContent === boxes[4].textContent && boxes[4].textContent === boxes[8].textContent)
+    {
+        console.log("Player " + boxes[0].textContent + " won!!!");
+    }
+    if (boxes[2].textContent !== "" && boxes[2].textContent === boxes[4].textContent && boxes[4].textContent === boxes[6].textContent)
+    {
+        console.log("Player " + boxes[2].textContent + " won!!!");
+    }
+}
+
+function endGame()
+{
+    if (boxes[0].textContent !== "" && boxes[0].textContent === boxes[1].textContent && boxes[1].textContent === boxes[2].textContent)
+    {
+        
+    }
+    if (moveCount === 9)
+    {
+        let message = document.querySelector("p");
+        message.textContent = "Tie Game (Cat's Game)!!"
+    }
 }
 
 document.addEventListener('DOMContentLoaded', function()
@@ -43,15 +89,13 @@ document.addEventListener('DOMContentLoaded', function()
     {
         box.addEventListener("click", function() 
         {
-            if (box.textContent !== "X" && box.textContent !== "O")
+            if (box.textContent !== "X" && box.textContent !== "O" box.textContent !== " ")
             {
                 box.textContent = whatMove();
                 moveCount++;
                 whoMove();
-                if (moveCount === 9)
-                {
-                    console.log("Game finished");
-                }
+                checkWins();
+                endGame();
             }
         });
     });
