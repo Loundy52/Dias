ae2d28da46c9756e724c5b6b100716b73f11e3ab
Alice Aekyung Huh <aliceaekyunglee@alices-imac.myfiosgateway.com>
2020-07-20 11:09:21 -0400

    [tik tak toe] fix result

--- js/app.js
+++ js/app.js
@@ -35,6 +35,8 @@ document.addEventListener('DOMContentLoaded', () => {
         //const playerO = document.querySelector('.playerO')
         const empty = null;
         
+
+        //result 
         if((squares[0]!== empty)&&(squares[0] == squares[1])&&(squares[1] == squares[2])){
             return 
             }else if ((squares[3]!== empty)&&(squares[3] == squares[4])&&(squares[4]== squares[5])){
@@ -48,11 +50,11 @@ document.addEventListener('DOMContentLoaded', () => {
             }else if((squares[2]!== empty)&&(squares[2] == squares[5])&&(squares[5]== squares[8])){
                 resultM ("You win!")
             }
-        else if(squares[0]&&squares[1]&&squares[2]&&squares[3]&&squares[4]&&squares[5]&&squares[6]&&squares[7]&&squares[8]== empty)
+        else if(squares[0]&&squares[1]&&squares[2]&&squares[3]&&squares[4]&&squares[5]&&squares[6]&&squares[7]&&squares[8] != empty)
                 resultM ("it's a tie")
 
             function resultM(result){
-                const resultText = document.createElement('p');
+                let resultText = document.createElement('p');
                 resultText.innerText = result;
                 document.body.appendChild(resultText);
             }    
@@ -66,71 +68,11 @@ document.addEventListener('DOMContentLoaded', () => {
     
 
 
-//const gridContainer = document.getElementsByClassName('grid-container');
-//console.log(gridContainer);
 
 
 
 
-//console.log(red);
 
-
-
-//const grid = [gridOne, gridTwo, gridThree, gridFour, gridFive, gridSix, gridSeven, gridEight, gridNine]
-
-//const playerRed = 'red'
-//const playerBlue = 'blue'
-
-//let switchTurn
-
-//grid.forEach(cell => {
-//})
-
-//function handleClick(e){
-//    const cell = e.target
-//    const currentTurn = switchTurn ? playerRed : playerBlue
-//    displayInput(cell, currentTurn)
-//    swapTurns()
-//}
-//
-//function displayInput(cell,currentTurn){
-//    cell.style.backgroundColor = 'red'
-//}
-//function swapTurns() {
-//    switchTurn = !switchTurn
-//}
-//console.log(grid);
-
-//function clickRed (){
-//    this.style.backgroundColor = 'red';
-//}
-//function clickBlue (){
-//    this.style.backgroundColor = 'blue';
-//}
-
-
-
-//displayInput(grid);
-
-//keepGoing = 1
-//while (keepGoing < 3){
-//    grid.forEach(i => i.addEventListener('click', () => i.style.background = "red"))
- //   grid.forEach(i => i.addEventListener('click', () => i.style.background = "red")) 
- //   keepGoing++} 
-
- //grid[i].addEventListener('click', clickRed);
- //grid[i].addEventListener('click', clickBlue);
-
-
- //End Game
-
- //if(totalMove = 0) {
- //    return ("It's a tie!")
- //}else if{
- //    if(grid.column1 = click; )
- //    else if(grid.column2 = click; )
- //    else if(grid..column3 = click;)
- //}
 
 
 
