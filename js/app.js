bb8203cccbcd0e83e298a193389262798bf1b52e
General Assembly <generalassembly@Davids-MacBook-Pro.local>
2020-07-24 08:59:11 -0700

    [wins] Add checkForWinner

--- js/app.js
+++ js/app.js
@@ -4,34 +4,60 @@
 //     oTurn.textContent= "O Turn";
 
 let jsBoxes = [];
+let Xs = [];
+let Os = [];
 const markXs = (event) => {
-    Xs.push(event.target);
-    jsBoxes.includes("X");
-    console.log(jsBoxes);
+    Xs.push(event);
+    console.log(Xs);
 };
 
 const markOs = (event) => {
-    Os.push(event.target);
-    
+    Os.push(event);
+    console.log(Os);
 }
 
-let Xs = [];
-let Os = [];
+const checkForWin = (arr) => {
+    if (arr.includes("cell0") && arr.includes("cell3") && arr.includes("cell")) {
+        return true;
+    } else if (arr.includes("cell1") && arr.includes("cell4") && arr.includes("cell7")) {
+        return true;
+    } else if (arr.includes("cell2") && arr.includes("cell5") && arr.includes("cell8")) {
+        return true;
+    } else if (arr.includes("cell0") && arr.includes("cell1") && arr.includes("cell2")) {
+        return true;
+    } else if (arr.includes("cell3") && arr.includes("cell4") && arr.includes("cell5")) {
+        return true;
+    } else if (arr.includes("cell6") && arr.includes("cell7") && arr.includes("cell8")) {
+        return true;
+    } else if (arr.includes("cell0") && arr.includes("cell4") && arr.includes("cell8")) {
+        return true;
+    } else if (arr.includes("cell2") && arr.includes("cell4") && arr.includes("cell6")) {
+        return true;
+    } else {
+        return false;
+    };
+}
+
+const markWinner = () =>{
+    
+}
 
 const whoseTurn = document.querySelector("#whoseTurn");
 
 let clicks = 0;
 
-const wins = [
-    ["cell0", "cell3", "cell6"],
-    ["cell1", "cell4", "cell7"],
-    ["cell2", "cell5", "cell8"],
-    ["cell0", "cell1", "cell2"],
-    ["cell3", "cell4", "cell5"],
-    ["cell6", "cell7", "cell8"],
-    ["cell0", "cell4", "cell8"],
-    ["cell2", "cell4", "cell6"]
-];
+// const wins = [
+//     ["cell0", "cell3", "cell6"],
+//     ["cell1", "cell4", "cell7"],
+//     ["cell2", "cell5", "cell8"],
+//     ["cell0", "cell1", "cell2"],
+//     ["cell3", "cell4", "cell5"],
+//     ["cell6", "cell7", "cell8"],
+//     ["cell0", "cell4", "cell8"],
+//     ["cell2", "cell4", "cell6"]
+// ];
+
+
 
 
 
@@ -45,18 +71,23 @@ whoseTurn.textContent = "X Turn";
 
 const turns = (event) => {
     clicks ++;
+    console.log(clicks);
     if (clicks % 2 !== 0) { 
-        whoseTurn.textContent = "X Turn";
+        whoseTurn.textContent = "O Turn";
         event.target.textContent = "X";
         event.target.classList = "playerX";
-        event.target.addEventListener("click", markXs);
+        markXs(event.target.id);
         event.target.removeEventListener("click", turns);
-    } else {
-        whoseTurn.textContent = "O Turn";
+        checkForWin(Xs);
+        checkForWin(Os);
+    } else if (clicks === 1 || clicks % 2 === 0) {
+        whoseTurn.textContent = "X Turn";
         event.target.textContent = "O";
         event.target.classList = "playerO";
-        event.target.addEventListener("click", markOs);
+        markOs(event.target.id);
         event.target.removeEventListener("click", turns);
+        checkForWin(Xs);
+        checkForWin(Os);
     }
 }
 
