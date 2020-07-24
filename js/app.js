3055ab8263bb01795012a4f1c0f78055011375c8
General Assembly <generalassembly@Davids-MacBook-Pro.local>
2020-07-23 21:02:29 -0700

    [wins] Create wins array

--- js/app.js
+++ js/app.js
@@ -5,11 +5,28 @@
 
 const whoseTurn = document.querySelector("#whoseTurn");
 
+const wins = [
+    ["cell0", "cell3", "cell6"],
+    ["cell1", "cell4", "cell7"],
+    ["cell3", "cell5", "cell8"],
+    ["cell0", "cell1", "cell2"],
+    ["cell3", "cell4", "cell5"],
+    ["cell6", "cell7", "cell8"],
+    ["cell0", "cell4", "cell8"],
+    ["cell2", "cell4", "cell6"]
+];
+
+
+
+const checkForWin = () => {
+
+}
+
 let clicks = 0;
 whoseTurn.textContent = "X Turn";
 
 const turns = (event) => {
-    clicks += 1;
+    clicks ++;
     if (clicks % 2 !== 0) { 
         whoseTurn.textContent = "X Turn";
         event.target.textContent = "X";
@@ -21,17 +38,27 @@ const turns = (event) => {
     }
 }
 
-// for loop iterates 9 times
+// let winner = false;
+
+// const reset = () => {
+//     if (winner === true || )
+// }
 
 const gameBoard = document.querySelector(".gameBoard");
 const jsBoxes = [];
 
 
+// for loop iterates 9 times
 for (let i = 0; i < 9; i++) {
+    // create js divs
     let box = document.createElement("div");
+    // assign each one a class of squares
     box.classList.add("squares");
+    // and add an id of the index
     box.id = "cell" + i;
+    // listen for the click and link it to turn count
     box.addEventListener("click", turns)
+    // add new divs to the gameboard
     gameBoard.appendChild(box);
     jsBoxes.push(box);
 }
