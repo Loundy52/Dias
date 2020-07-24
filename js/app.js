ad0a081fdf7f03bf482a891a39cb32a21b05a1ba
Alpha Martinez <alphamartinez@Alphas-MacBook-Pro.local>
2020-07-23 20:14:46 -0400

    added photos to my players class

--- js/app.js
+++ js/app.js
@@ -15,60 +15,55 @@ document.addEventListener('DOMContentLoaded', function (){
     //functions 
     let winner = false;
     let turn = 0;
-
+    buttonClicked = 0;
+    let boxClicked = false;
 
     gameBoard.addEventListener('click', event => {
         
-        //ifClicked = 0;
-
         let cell = event.target;//which cell was clicked on...set an attribute
-        //console.log(event.target);
-        if (cell.className == 'block'){
+           
+              if(boxClicked == 0) {  
+                if (turn % 2 == 0){
+                    turn++;
+                    cell.setAttribute('class', 'player1')
+                    //look up attribute (TA mentioned to add this)
+                    
+                    cell.toggleAttribute('block');
+                    switchOver.textContent = "Player 2 go!"
+                   
+                  
+                    winState();
+    
+                    //tie();
+    
+                } else {
+                    turn++;
 
-            if (turn % 2 == 0){
-                turn++;
-                cell.setAttribute('block', 'player1')
-                cell.textContent = player1;//look up attribute (TA mentioned to add this)
-                //icClicked = 1;
-                cell.toggleAttribute('block', 'player1');
-                switchOver.textContent = "Player 2 go!"
-                //console.log(switchOver);
-
-                winState();
-
-                //tie();
-
-            } else {
-                turn++;
-                cell.textContent = player2;
-                //ifClicked = 1;
-                cell.setAttribute('block', 'player2'); //look up attribute (TA mentioned to add this)
-                cell.toggleAttribute('block'); //which led to using toggle attribute. still need to look up to fuly grap
-                //console.log(cell);
-                switchOver.textContent = "Player 1, we don't have all day!";
-
-                winState();
-
-                //tie();
-            };
-
-
-        }
+                        cell.setAttribute('class', 'player2'); //look up attribute (TA mentioned to add this)
+                        cell.toggleAttribute('block'); //which led to using toggle attribute. still need to look up to fuly grap
+                  
+                    switchOver.textContent = "Player 1, we don't have all day!";
+                  
+                    winState();
+    
+                    
+                };
+    
+            }
+    
 
         function winState() { //what is .className -> refresh is it a property calling to element.class
             let b1 = document.getElementById("block1"); //short names are best
             let b2 = document.getElementById("block2");
-            //console.log(b2);
             let b3 = document.getElementById("block3");
-            //console.log(b3);
-            let b4 = document.getElementById("block4");
+            let b4 = document.getElementById("block4"); //tried className but no luck. try elementById?
             let b5 = document.getElementById("block5");
             let b6 = document.getElementById("block6");
             let b7 = document.getElementById("block7");
             let b8 = document.getElementById("block8");
             let b9 = document.getElementById("block9");
     
-    
+            
     
         }
     
